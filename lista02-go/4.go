de3f3f6f529aa8e2a6b1e56f86ccb578cb955288
daviddaves1 <142112857+daviddaves1@users.noreply.github.com>
2024-04-26 14:16:01 -0300

    4.go

--- lista02-go/4.go
+++ lista02-go/4.go
@@ -5,62 +5,64 @@ import (
 )
 
 func main() {
-	var n, i, s float64
-	var k int
+	var n, i, s float64 = 0, 0, 0
+	var k int = 0
 
-	fmt.Printf("_______________________________________________________________________\n")
-	fmt.Printf("Insira os valores de N, I, e S: ")
-	fmt.Scan(&n, &i, &s)
-
-	for n < 0 || n > 9 {
-		fmt.Printf("N deve estar entre 0 e 9. Digite o valor de N novamente: ")
-		fmt.Scan(&n)
-	}
-
-	fmt.Printf("Insira o valor de K: ")
-	fmt.Scan(&k)
-	fmt.Printf("_______________________________________________________________________\n\n\n\n")
-
-	fmt.Printf("==================================================================================================\n")
-
-	if n >= 0 && n <= 9 {
-		fmt.Println("Tabuada de soma:")
-		fmt.Printf("%.2f + %.2f = %.2f\n", n, i, n+i)
-		for index:=1; index<k; index++ {
-			i += s
+		fmt.Printf("\n\n")
+		fmt.Printf("_______________________________________________________________________\n")
+		fmt.Printf("Insira os valores de N, I, e S: ")
+		fmt.Scan(&n, &i, &s)
+		for n < 0 || n > 9 {
+			fmt.Printf("N deve estar entre 0 e 9. Digite o valor de N novamente: ")
+			fmt.Scan(&n)
+		}			
+		fmt.Printf("Insira o valor de K: ")
+		fmt.Scan(&k)
+		fmt.Printf("_______________________________________________________________________\n\n\n\n")
+		
+		const value, zero = 1, 0
+		temp := s
+		
+		fmt.Printf("==================================================================================================\n")
+		if n >= 0 && n <= 9 {
+			fmt.Printf("Tabuada de soma:\n")
 			fmt.Printf("%.2f + %.2f = %.2f\n", n, i, n+i)
-		}
-		fmt.Printf("\n")
-
-		i = 0
-		fmt.Printf("Tabuada de subtração:\n")
-		fmt.Printf("%.2f - %.2f = %.2f\n", n, i, n-i)
-		for index:=1; index<k; index++ {
-			i += s
+			for index:=value; index<k; index++ {
+				if index >= 1 {
+					fmt.Printf("%.2f + %.2f = %.2f\n", n, i+s, n+(i+s))
+					s += temp
+				}
+			}
+			fmt.Printf("\n")
+			fmt.Printf("Tabuada de subtração:\n")
 			fmt.Printf("%.2f - %.2f = %.2f\n", n, i, n-i)
-		}
-		fmt.Printf("\n")
-
-		i = 1
-		fmt.Printf("Tabuada de multiplicação:\n")
-		fmt.Printf("%.2f x %.2f = %.2f\n", n, i, n*i)
-		for index:=1; index<k; index++ {
-			i += s
+			s = zero
+			for index:=value; index<k; index++ {
+				if index >= 1 {
+					fmt.Printf("%.2f - %.2f = %.2f\n", n, i+s, n-(i+s))
+					s += temp
+				}
+			}
+			fmt.Printf("\n")
+			fmt.Printf("Tabuada de multiplicação:\n")
 			fmt.Printf("%.2f x %.2f = %.2f\n", n, i, n*i)
-		}
-		fmt.Printf("\n")
-
-		i = 1
-		fmt.Printf("Tabuada de divisão:\n")
-		fmt.Printf("%.2f / %.2f = %.2f\n", n, i, n/i)
-		for index:=1; index<k; index++ {
-			i += s
-			if i != 0 {
-				fmt.Printf("%.2f / %.2f = %.2f\n", n, i, n/i)
-			} else {
-				fmt.Printf("Impossível dividir por zero\n")
+			s = zero
+			for index:=value; index<k; index++ {
+				if index >= 1 {
+					fmt.Printf("%.2f x %.2f = %.2f\n", n, i+s, n*(i+s))
+					s += temp
+				}      
+			}
+			fmt.Printf("\n")
+			fmt.Printf("Tabuada de divisão:\n")
+			fmt.Printf("%.2f / %.2f = %.2f\n", n, i, n/i)
+			s = zero
+			for index:=value; index<k; index++ {
+				if index >= 1 {
+					fmt.Printf("%.2f / %.2f = %.2f\n", n, i+s, n/(i+s))
+					s += temp
+				}
 			}
 		}
-	}
-	fmt.Println("==================================================================================================")
+		fmt.Printf("==================================================================================================\n")
 }
